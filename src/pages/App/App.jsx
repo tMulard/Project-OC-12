e3c8ccb22dcbba5dfb2d73f8c5e542392f564a8c
tMulard <theo.mulard@gmail.com>
2024-11-22 13:12:58 +0100

    Upgraded the front page display

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import { Link } from 'react-router-dom'
 
+import logo from "../../../public/Icons/logo_SportSee.png"
+
 const App = () => {
 
   return (
    <div className='proxy'>
+      <img src={logo} alt="logo" />
+      <h2>Connectez-vous :</h2>
       <Link to='/user/12/profile'>Karl</Link>
       <Link to='/user/18/profile'>Cecilia</Link>
    </div>
